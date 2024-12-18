d7253ff96aaa521985da69d36c33d41e60a5ef54
Masri <masrialemu404@gmail.com>
2024-12-18 11:55:27 +0300

    git add . && git commit -m "Add user registration API" && git push origin main

---
+++ myproject/chatapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=128)
+    tokens = models.IntegerField(default=4000)
+
+    def __str__(self):
+        return self.username
